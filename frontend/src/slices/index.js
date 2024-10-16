08805b6cd0c386251a140848d5eb797887a804d3
AnastasiiaSleptsova <am89650731404@gmail.com>
2024-10-16 14:21:10 +0300

    add the signup reducer to the store

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -2,16 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import { chatsApi } from "../api/chatsApi";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { messagesApi } from "../api/messagesApi";
+import { signupUserApi } from "../api/signupUserApi";
 
 export const store = configureStore({
   reducer: {
     [chatsApi.reducerPath]: chatsApi.reducer,
     [messagesApi.reducerPath]: messagesApi.reducer,
+    [signupUserApi.reducerPath]: signupUserApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(chatsApi.middleware)
-      .concat(messagesApi.middleware),
+      .concat(messagesApi.middleware)
+      .concat(signupUserApi.middleware),
   devTools: true, // TODO убрать в прод сборке
 });
 
