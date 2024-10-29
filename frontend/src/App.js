e564e420f7762001a5b0c339662fdde097cfbfe0
AnastasiiaSleptsova <am89650731404@gmail.com>
2024-10-29 16:22:28 +0300

    add rollbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,24 +3,49 @@ import { Router } from "./components/app/Router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Header } from "./components/pages/Header/Header";
 import { Footer } from "./components/pages/Footer/Footer";
+import React from "react";
+import { Provider, ErrorBoundary } from "@rollbar/react"; 
 
 import classes from "./App.module.css";
 
+const rollbarConfig = {
+  accessToken: "POST_CLIENT_ITEM_ACCESS_TOKEN",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  environment: "production",
+  server: {
+    root: "http://example.com/",
+    branch: "main",
+  },
+  code_version: "0.13.7",
+  payload: {
+    person: {
+      id: 117,
+      email: "chief@unsc.gov",
+      username: "john-halo",
+    },
+  },
+};
+
 function App() {
   return (
-    <div className={classes.app}>
-      <BrowserRouter>
-        <div className={classes.wrapper}>
-          <Header />
-          <div className={classes.content}>
-            <Router />
-          </div>
-          <footer className={classes.footer}>
-            <Footer />
-          </footer>
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>
+        <div className={classes.app}>
+          <BrowserRouter>
+            <div className={classes.wrapper}>
+              <Header />
+              <div className={classes.content}>
+                <Router />
+              </div>
+              <footer className={classes.footer}>
+                <Footer />
+              </footer>
+            </div>
+          </BrowserRouter>
         </div>
-      </BrowserRouter>
-    </div>
+      </ErrorBoundary>
+    </Provider>
   );
 }
 
